985935848f2150254df32f9174fe7d1e6788edf6
Nada Mohammed <naddaa26@gmail.com>
2024-02-07 16:18:19 +0200

    added project

---
+++ README.md
@@ -0,0 +1,16 @@
+# Meals-App
+
+## Description 
+
+- A meal application that provides details about the recipe and ingredients necessary for meals based on their category. Additionally, it allows the user to mark meals as favorites and filter meals based on their ingredients.
+
+## Technologies used
+
+- [Flutter](https://flutter.dev/): An open-source UI software development kit created by Google used to develop cross platform applications.
+
+## Preview
+
+- <img src="/img/main.png" alt="Alt text" title="Main_Screen" height="500" style="padding-right: 20px;"> <img src="/img/meals.png" alt="Alt text" title="Meals_Screen" height="500" style="padding-right: 20px;">    <img src="/img/meals 2.png" alt="Alt text" title="Meals_Screen" height="500">
+
+- <img src="/img/meal details.png" alt="Alt text" title="Meal_Details_Screen" height="500" style="padding-right: 20px;"> <img src="/img/side bar.png" alt="Alt text" title="Side_Bar" height="500" style="padding-right: 20px;">    <img src="/img/filters.png" alt="Alt text" title="Filters_Screen" height="500">
+
---
+++ lib/screens/categories.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
+import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meals.dart';
+import 'package:meals/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatefulWidget {
+  const CategoriesScreen({
+    super.key,
+    required this.availableMeals,
+  });
+
+  final List<Meal> availableMeals;
+
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin { // explicit animation
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = widget.availableMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+    // Alternative => Navigator.of(context).push(route);
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (ctx) => MealsScreen(
+                  title: category.title,
+                  meals: filteredMeals,
+                )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, // left to right
+          childAspectRatio: 3 / 2, // size of grid view items
+          crossAxisSpacing: 20, // space bet columns horixontally
+          mainAxisSpacing: 20, // space bet columns vertically
+        ),
+        children: [
+          // Alternative option
+          // avaialableCategories.map((category) => CategoryGridItem(category: category))
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () => {_selectCategory(context, category)},
+            )
+        ],
+      ),
+      builder: (context, child) => SlideTransition(
+        position: Tween(
+          begin: const Offset(0, 0.3), // push down
+          end: const Offset(0, 0),
+        ).animate(CurvedAnimation(
+          parent: _animationController,
+          curve: Curves.easeInOut,
+        )),
+        child: child,
+      ),
+    );
+  }
+}
